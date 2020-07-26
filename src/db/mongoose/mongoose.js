a7bbc5f31a95c70028d0409cf5146e453feb90c5
Nhat Tran <nhattran.net91@gmail.com>
2020-07-26 21:03:06 +0700

    Task-Manager: Add user.js

--- src/db/mongoose/mongoose.js
+++ src/db/mongoose/mongoose.js
@@ -15,36 +15,6 @@ const Task = mongoose.model("Task", {
     }
 });
 
-const User = mongoose.model("User", {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        trim: true,
-        required: true,
-        lowerCase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error("The input value is not a valid email");
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        minlength: 6,
-        trim: true,
-        validate(value) {
-            if (value.includes("password")) {
-                throw new Error(`The password contains \"password\"`)
-            }
-        }
-    }
-})
-
 var task = new Task({
     description: " Do something   "
 });
---
+++ src/models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager", { useNewUrlParser: true });
+
+const User = mongoose.model("User", {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        lowerCase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("The input value is not a valid email");
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+        trim: true,
+        validate(value) {
+            if (value.includes("password")) {
+                throw new Error(`The password contains \"password\"`)
+            }
+        }
+    }
+})
+
+module.exports = User;
