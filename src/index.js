7dab897c6dc315eeebffe4a1ac468d968cf04643
Nhat Tran <nhattran.net91@gmail.com>
2020-07-26 21:47:37 +0700

    Task-Manager: Setup Rest Api to add User

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const User = require("./models/user");
 
 const app = express();
+app.use(express.json());
+
 const port = process.env.PORT || 3000;
 
 app.post("/users", (req, res) => {
-    res.send("Testing")
+    var user = new User(req.body);
+    user.save().then(result => res.send(result))
+        .catch(error => res.status("400").send(error));
 })
 
 app.listen(port, () => {
