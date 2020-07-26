881dc00a7b0d47f0f8cb77e20c33bb9b7a070566
Nhat Tran <nhattran.net91@gmail.com>
2020-07-26 11:01:51 +0700

    Task-Manager: Update how to add Task model

--- src/db/mongoose/mongoose.js
+++ src/db/mongoose/mongoose.js
@@ -1,18 +1,18 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/task-manager", { useNewUrlParser: true }).then(() => {
-    const Task = mongoose.model("Task", {
-        completed: {
-            type: Boolean
-        },
-        description: {
-            type: String
-        }
-    });
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager", { useNewUrlParser: true });
 
-    var task = new Task();
-    task.description = "Practicing NodeJS";
-    task.completed = false;
-    task.save().then(result => console.log("Success: " + result))
-        .catch(error => console.log("Error: " + error));
-})
+const Task = mongoose.model("Task", {
+    completed: {
+        type: Boolean
+    },
+    description: {
+        type: String
+    }
+});
+
+var task = new Task();
+task.description = "Practicing NodeJS";
+task.completed = false;
+task.save().then(result => console.log("Success: " + result))
+    .catch(error => console.log("Error: " + error));
