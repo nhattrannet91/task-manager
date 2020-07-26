9647a32ce0e15fede470ef4dc19771bafd13ad34
Nhat Tran <nhattran.net91@gmail.com>
2020-07-26 13:17:32 +0700

    Task-Manager: Add User model as well as its validator

--- src/db/mongoose/mongoose.js
+++ src/db/mongoose/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager", { useNewUrlParser: true });
 
@@ -11,8 +12,46 @@ const Task = mongoose.model("Task", {
     }
 });
 
-var task = new Task();
-task.description = "Practicing NodeJS";
-task.completed = false;
-task.save().then(result => console.log("Success: " + result))
+const User = mongoose.model("User", {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        lowerCase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("The input value is not a valid email");
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+        trim: true,
+        validate(value) {
+            if (value.includes("password")) {
+                throw new Error(`The password contains \"password\"`)
+            }
+        }
+    }
+})
+// var task = new Task();
+// task.description = "Practicing NodeJS";
+// task.completed = false;
+// task.save().then(result => console.log("Success: " + result))
+// .catch(error => console.log("Error: " + error));
+
+var user = new User({
+    name: " Nhat Tran  ",
+    email: "abc@eblca.vn",
+    password: "fdsffdsfdspasswor"
+})
+
+user.save().then(result => console.log("Success: " + result))
     .catch(error => console.log("Error: " + error));
