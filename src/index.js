e2e310610b151fb9a70a4fccee36ad93a425e3f0
Nhat Tran <nhattran.net91@gmail.com>
2020-07-27 14:30:47 +0700

    Task-Manager: Add two read User(s) requests

--- src/index.js
+++ src/index.js
@@ -14,6 +14,16 @@ app.post("/users", (req, res) => {
         .catch(error => res.status("400").send(error));
 })
 
+app.get("/users", (req, res) => {
+    User.find({}).then(users => res.send(users))
+        .catch(error => res.status(500).send());
+})
+
+app.get("/users/:id", (req, res) => {
+    User.findById(req.params.id).then(user => res.send(user))
+        .catch(error => res.status(500).send());
+})
+
 app.post("/tasks", (req, res) => {
     var task = new Task(req.body);
     task.save().then(result => res.status(201).send(result))
