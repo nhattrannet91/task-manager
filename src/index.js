0a9253d3cc8d2aaed4167ddd798fda4fc4e796c4
Nhat Tran <nhattran.net91@gmail.com>
2020-07-28 14:26:58 +0700

    Task-Manager: Update task apis with async/await

--- src/index.js
+++ src/index.js
@@ -40,27 +40,37 @@ app.get("/users/:id", async (req, res) => {
     }
 })
 
-app.post("/tasks", (req, res) => {
+app.post("/tasks", async (req, res) => {
     var task = new Task(req.body);
-    task.save().then(result => res.status(201).send(result))
-        .catch(error => res.status("400").send(error));
+    try {
+        const result = await task.save();
+        res.status(201).send(result);
+    } catch (error) {
+        res.status("400").send(error)
+    }
 })
 
-app.get("/tasks", (req, res) => {
-    Task.find({}).then(tasks => res.send(tasks))
-        .catch(error => res.status(500).send());
+app.get("/tasks", async (req, res) => {
+    try {
+        const tasks = await Task.find({});
+        res.send(tasks);
+    } catch (error) {
+        res.status(500).send()
+    }
 })
 
-app.get("/tasks/:id", (req, res) => {
-    Task.findById(req.params.id).then(task => {
+app.get("/tasks/:id", async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.id);
         if (!task) {
             console.log("Task not found");
             return res.status(404).send();
         }
 
         res.send(task);
-    })
-        .catch(error => res.status(500).send(error));
+    } catch (error) {
+        res.status(500).send(error)
+    }
 })
 
 app.listen(port, () => {
