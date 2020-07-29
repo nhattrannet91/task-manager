f94647e430f2024416556994cb0623983b22c3fe
Nhat Tran <nhattran.net91@gmail.com>
2020-07-29 10:43:30 +0700

    Task-Manager: Add delete apis

--- src/index.js
+++ src/index.js
@@ -59,6 +59,19 @@ app.patch("/users/:id", async (req, res) => {
     }
 })
 
+app.delete("/users/:id", async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return res.status(404).send({error: "User not found"});
+        }
+
+        res.send(user);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
 app.post("/tasks", async (req, res) => {
     var task = new Task(req.body);
     try {
@@ -108,6 +121,19 @@ app.patch("/tasks/:id", async (req, res) => {
         res.send(task);
     } catch (error) {
         res.status(400).send();
+    }
+})
+
+app.delete("/tasks/:id", async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id);
+        if (!task) {
+            return res.status(404).send({error: "Task not found"});
+        }
+
+        res.send(task);
+    } catch (error) {
+        res.status(500).send(error);
     }
 })
 
