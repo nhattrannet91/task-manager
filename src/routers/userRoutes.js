3416f210be668c4be5f44df8a0ef111626d37a2f
Nhat Tran <mnt@ELCA.vn>
2020-08-04 20:54:39 +0700

    Task-Manager: Change get users to get user profile

--- src/routers/userRoutes.js
+++ src/routers/userRoutes.js
@@ -14,13 +14,8 @@ router.post("/users", async (req, res) => {
     }
 })
 
-router.get("/users", auth, async (req, res) => {
-    try {
-        const users = await User.find({});
-        res.send(users);
-    } catch (error) {
-        res.status(500).send()
-    }
+router.get("/users/me", auth, async (req, res) => {
+    res.send(req.user);
 })
 
 router.get("/users/:id", async (req, res) => {
