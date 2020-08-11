6bbdbd557ad3e068b0c08d2b1454fd59a7c187b1
Nhat Tran <mnt@ELCA.vn>
2020-08-11 22:16:00 +0700

    Task-Manager: Implement sort, pagination, filter

--- src/routers/taskRoutes.js
+++ src/routers/taskRoutes.js
@@ -13,11 +13,35 @@ router.post("/tasks", auth, async (req, res) => {
     }
 })
 
+// GET /tasks?completed=true
+// GET /tasks?limit=1
+// GET /tasks?skip=1
+// GET /tasks?sort=createdAt:desc
 router.get("/tasks", auth, async (req, res) => {
     try {
-        await req.user.populate("tasks").execPopulate();
+        const match = {};
+        const sort = {};
+        if (req.query.completed) {
+            match.completed = req.query.completed === "true";
+        }
+
+        if(req.query.sort){
+            const parts = req.query.sort.split(":");
+            sort[parts[0]] = parts[1] === "asc" ? 1 : -1;
+        }
+
+        await req.user.populate({
+            path: "tasks",
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
+        }).execPopulate();
         res.send(req.user.tasks);
     } catch (error) {
+        console.log(error);
         res.status(500).send()
     }
 })
@@ -31,7 +55,7 @@ router.patch("/tasks/:id", auth, async (req, res) => {
     }
 
     try {
-        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id});
+        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id });
         if (!task) {
             return res.status(400).send("Task not found");
         }
@@ -40,13 +64,13 @@ router.patch("/tasks/:id", auth, async (req, res) => {
         await task.save();
         res.send(task);
     } catch (error) {
-        res.status(400).send({error});
+        res.status(400).send({ error });
     }
 })
 
 router.delete("/tasks/:id", auth, async (req, res) => {
     try {
-        const task = await Task.deleteOne({ _id: req.params.id, owner: req.user._id});
+        const task = await Task.deleteOne({ _id: req.params.id, owner: req.user._id });
         if (!task) {
             return res.status(404).send({ error: "Task not found" });
         }
