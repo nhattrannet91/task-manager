19427a9ecec9119ef50fe402451797edfa0982ac
Nhat Tran <nhattran.net91@gmail.com>
2020-08-15 21:53:44 +0700

    Task-Manager: Add get avatar route

--- src/routers/userRoutes.js
+++ src/routers/userRoutes.js
@@ -87,6 +87,20 @@ router.delete("/users/me/avatar", auth, async (req, res) => {
     res.send();
 })
 
+router.get("/users/:id/avatar", async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if(!user || !user.avatar){
+            throw new Error();
+        }
+
+        res.set("Content-Type", "image/jpeg");
+        res.send(user.avatar);
+    } catch (error) {
+       res.status(404).send();
+    }
+})
+
 router.post("/users/logout", auth, async (req, res) => {
     try {
         req.user.tokens = req.user.tokens.filter(x => x.token !== req.token);
