e4afa7c307a6f9bf673738466b9c51cd9db0867f
Nhat Tran <nhattran.net91@gmail.com>
2020-08-16 14:15:44 +0700

    Task-Manager: Add mongodb configs to suppress warning messages

--- src/db/mongoose/mongoose.js
+++ src/db/mongoose/mongoose.js
@@ -1,2 +1,3 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/task-manager", { useNewUrlParser: true });
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
+);
